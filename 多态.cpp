3ad586fce8bf71dec94d8b584b62476886ed1cef
Sherlock Guan <46273832+sherlockguan@users.noreply.github.com>
2020-04-01 21:01:29 +0900

    Create 多态.cpp

---
+++ 多态.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+//菱形继承是解释不清的一特殊情况，父母virtual,祖父不加 ，其他同名函数的情况才基类virtual
+using namespace std;
+class Human
+{
+public:
+	virtual void makeFriend()
+	{
+		cout << "huMan make friend" << endl;
+	}
+};
+class Girl :public Human
+{
+public:
+	void makeFriend()
+	{
+		cout << "girl make Friend" << endl;
+	}
+};
+class Boy :public Human
+{
+public:
+	void makeFriend()
+	{
+		cout << "boy make Friend" << endl;
+	}
+};
+
+void test(Human* object)
+{
+	object->makeFriend();
+}
+
+int main()
+{
+	Human* pHuman = new Human;
+	Girl *pGirl = new Girl;
+	Boy *pBoy = new Boy;
+	test(pHuman);
+	test(pGirl);
+	test(pBoy);
+	//课本上一般的写法， virtual 多态防止覆盖,否则都是调用父类，不能实现同一行为不同结果
+	//1.必须父类存在虚函数
+	//2.存在父类指针对子类对象的引用
+	//2.1 new语法  2.2 取对象的地址
+	Human* pParent = new Girl;
+	pParent->makeFriend();
+	pParent = new Boy;
+	pParent->makeFriend();
+	Girl objectGirl;
+	pParent = &objectGirl;
+	pParent->makeFriend();
+	Boy objectBoy;
+	pParent = &objectBoy;
+	pParent->makeFriend();
+	/*
+	那个指针调用哪个东西
+	在没有virtual的父类被继承下
+	1.父类指针被子类对象初始化
+	2.永远只能调用父类的成员
+
+	在存在virtual情况下,存在父类指针被子类对象的初始化
+	1.指针调用的成员是根据子类对象
+	*/
+	system("pause");
+	return 0;
+}
+
