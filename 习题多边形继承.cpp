a036fcd33efaed2504d6c8c507dd384bf143771d
Sherlock Guan <46273832+sherlockguan@users.noreply.github.com>
2020-04-01 20:24:49 +0900

    Update 习题多边形继承.cpp

--- 习题多边形继承.cpp
+++ 习题多边形继承.cpp
@@ -75,4 +75,77 @@ int main()
 }
 
 
-//亲爱的小鬼老师， 我写是写了， 怎么也打印不出值来，辛苦老师了，都快结课了。。
+//亲爱的小鬼老师， 我写是写了， 怎么也打印不出值来，辛苦老师
+#include<iostream>
+#include<string>
+#define _USE_MATH_DEFINES
+#include<math.h>
+using namespace std;
+class polygon
+{
+public:
+
+	void print()
+	{
+		cout << "this is a abstract base class" << endl;
+	}
+	//polygon(){}
+	void printArea(int result)
+	{
+		cout << "the area of this polygon is:" << result << endl;
+	}
+	void printPerimeter(int result)
+	{
+		cout << "the perimeter of this polygon is:" << result << endl;
+	}
+	//protected:
+	//	int area;
+	//	int perimeter;
+};
+
+class rectangle :public polygon
+{
+public:
+	rectangle(int a, int b) :a(a), b(b)
+	{
+		this->a = a;
+		this->b = b;
+	}
+	int myArea()
+	{
+		return a*b;
+	}
+protected:
+	int a;
+	int b;
+};
+class circle :public polygon
+{
+public:
+
+	circle(int radius) :radius(radius)
+	{
+		this->radius = radius;
+	}
+
+	double myPerimeter()
+	{
+		return 2 * M_PI*radius;
+
+	}
+protected:
+	int radius;
+
+};
+int main()
+{
+
+	circle *pCircle = new circle(5);
+	pCircle->printPerimeter(pCircle->myPerimeter());
+
+	rectangle object(2, 3);
+	object.printArea(object.myArea());
+
+	system("pause");
+	return 0;
+}
