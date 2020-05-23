8013d011cd6d690cc0df9090a70027aa1de011a5
Sherlock Guan <46273832+sherlockguan@users.noreply.github.com>
2020-05-23 14:48:38 +0900

    Create resource.h

---
+++ windows/win32calculator/resource.h
@@ -0,0 +1,38 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 Win32Calc.rc 使用
+//
+#define IDD_MAIN_DLG                    101
+#define IDI_ICON1                       102
+#define IDC_BTN_BACK                    1002
+#define IDC_BTN_CLEAR                   1003
+#define IDC_BTN_1                       1004
+#define IDC_BTN_2                       1005
+#define IDC_BTN_3                       1006
+#define IDC_BTN_4                       1007
+#define IDC_BTN_5                       1008
+#define IDC_BTN_6                       1009
+#define IDC_BTN_7                       1010
+#define IDC_BTN_8                       1011
+#define IDC_BTN_9                       1012
+#define IDC_BTN_ADD_SUM                 1013
+#define IDC_BTN_0                       1014
+#define IDC_BTN_DOT                     1015
+#define IDC_BTN_CHU                     1016
+#define IDC_BTN_MUL                     1017
+#define IDC_BTN_SUB                     1018
+#define IDC_BTN_ADD                     1019
+#define IDC_BTN_EQUAL                   1020
+#define IDC_STATIC_RESULT               1021
+#define IDC_STATIC_SHOW                 1022
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1023
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
